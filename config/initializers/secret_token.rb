f595b07fb011e28461b827a8be0bc3962fa7ad12
chao <chao@snappytv.com>
2013-02-12 10:37:53 -0800

    for demo app not snappytv

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '08f0464b888b9d64061acb0aa0c821f4cfcc278f8636265616e8ec8486cff92e161d155463896237c1497d5bc9f460465a94ed863a3667784d55e7479a8a6c4d'
